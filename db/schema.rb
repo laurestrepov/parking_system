a5f07b6cd9cf4283b53f0ae8461a842d98bf1b66
LAURESTREPOV <lau.restrepo.v@gmail.com>
2016-07-18 20:12:20 -0500

    Se crea modelo para user a partir de devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714014240) do
+ActiveRecord::Schema.define(version: 20160719005328) do
 
   create_table "locations", force: :cascade do |t|
     t.float    "location_latitude"
@@ -48,9 +48,20 @@ ActiveRecord::Schema.define(version: 20160714014240) do
     t.string   "email"
     t.date     "birthdate"
     t.boolean  "is_admin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "vehicles", force: :cascade do |t|
     t.string   "model"
