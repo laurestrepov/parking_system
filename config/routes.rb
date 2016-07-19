58ea54a0d3c23ba4f0d630d991f50645d1de19b6
LAURESTREPOV <lau.restrepo.v@gmail.com>
2016-07-18 19:09:18 -0500

    Se crean rutas internas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  #root 'users#show'
+  #devise_for :users
+
+  resources :users do
+    resources :vehicles
+  end
+  resources :locations do
+    resources :slots
+  end
+
+#  resources :slots do
+#    resousrces :park_records
+#  end
+
+#  resousrces :vehicles do
+#    resources :park_records
+#  end
+
+#  resources :photos
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
