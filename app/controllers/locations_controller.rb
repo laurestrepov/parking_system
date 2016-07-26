0df64ebb6e7b89d11ded80817e4002be5101b326
LAURESTREPOV <lau.restrepo.v@gmail.com>
2016-07-25 23:02:26 -0500

    bug fixed new location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,4 +8,20 @@ class LocationsController < ApplicationController
     @locations = Location.all
     flash[:error] = 'No locations registered.' if @locations.empty?
   end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      flash[:success] = 'Location registered successfully.'
+      redirect_to locations_path
+    else
+      flash[:error] = @location.errors.full_messages.join(',')
+      render 'new'
+    end
+  end
+
+  def location_params
+    params.require(:location).permit(:id, :name, :latitude, :longitude, :address, :max_slots, :description)
+  end
+  private :location_params
 end
