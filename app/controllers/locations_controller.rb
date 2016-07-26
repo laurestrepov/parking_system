2b2173db59c985fc4ce7c23f19d73a00e4b00884
LAURESTREPOV <lau.restrepo.v@gmail.com>
2016-07-25 22:11:06 -0500

    se corrigen errores previos a deploy

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,4 +2,9 @@ class LocationsController < ApplicationController
   def new
     @location = Location.new
   end
+
+  def index
+    @locations = Location.all
+    flash[:error] = 'No locations registered.' if @locations.empty?
+  end
 end
