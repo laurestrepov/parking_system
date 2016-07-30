282e45a55805876a75e1422836889c5884579cfd
LAURESTREPOV <lau.restrepo.v@gmail.com>
2016-07-29 20:09:47 -0500

    se envía mail para confirmar creación de vehiculo

--- app/mailers/vehicle_mailer.rb
+++ app/mailers/vehicle_mailer.rb
@@ -1,10 +1,15 @@
 class VehicleMailer < ApplicationMailer
-  default from: 'notifications@example.com'
+  default from: 'notifications@placeyowheels.com'
 
   def new_vehicle_email(user, vehicle)
     @user = user
     @vehicle = vehicle
-    @url  = “http://localhost:3000/users/#{@user.id}/vehicles”
+    if Rails.env == 'development'
+      @url  = "http://localhost:3000/users/#{@user.id}/vehicles"
+    elsif Rails.env == 'production'
+      @url  = "http://place-yo-wheels.herokuapp.com/users/#{@user.id}/vehicles"
+    end
+
     mail(to: @user.email, subject: 'New Vehicle Registered.')
   end
 
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#config.action_mailer.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'lau.restrepo.v',
+  :password => '<3nomelase1991',
+  :domain => 'place-yo-wheels.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
