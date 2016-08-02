58a0fa6e1acbf2128a8d58fab992cdaf8cc24b62
LAURESTREPOV <lau.restrepo.v@gmail.com>
2016-08-01 20:22:22 -0500

    Se agregan y programan botones de Edit/delete en Locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,20 +3,45 @@ class LocationsController < ApplicationController
     @location = Location.new
   end
 
-
   def index
     @locations = Location.all
     flash[:error] = 'No locations registered.' if @locations.empty?
   end
   def create
-      @location = Location.new(location_params)
-      if @location.save
-        flash[:success] = 'Location registered successfully.'
-        redirect_to locations_path
-      else
-        flash[:error] = @location.errors.full_messages.join(',')
-        render 'new'
-      end
+    @location = Location.new(location_params)
+    if @location.save
+      flash[:success] = 'Location registered successfully.'
+      redirect_to locations_path
+    else
+      flash[:error] = @location.errors.full_messages.join(',')
+      render 'new'
+    end
+  end
+  
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    if @location.update_attributes(location_params)
+      flash[:success] = 'Location updated successfully.'
+      redirect_to locations_path
+    else
+      flash[:error] = @location.errors.full_messages.join(',')
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+    if @location.destroy
+      flash[:success] = 'Location deleted successfully.'
+      redirect_to locations_path
+    else
+      flash[:error] = @location.errors.full_messages.join(',')
+      render 'index'
+    end
   end
 
   def location_params
